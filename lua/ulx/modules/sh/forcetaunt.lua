2fde935be0a62f421f177fca4bf17a8a54839943
therealist <schtherealist@hotmail.com>
2014-07-24 06:44:54 -0500

    Update forcetaunt.lua

--- lua/ulx/modules/sh/forcetaunt.lua
+++ lua/ulx/modules/sh/forcetaunt.lua
@@ -10,11 +10,15 @@ function ulx.forcetaunt( calling_ply, target_plys, taunt )
 		elseif v:Team() == TEAM_HUNTERS then
 			random_taunt = table.Random(HUNTER_TAUNTS)
 			v:EmitSound(random_taunt, 100)
+		elseif v:Team() == TEAM_SPECTATOR then
+			ULib.tsayError( v, "This player is dead or spectating." )
+		elseif v:Team() == TEAM_UNASSIGNED then
+			ULib.tsayError( v, "This player is dead or spectating." )
 		end
 		
 	else 
 	
-	ULib.tsayError( calling_ply, "This player is dead or spectating." )
+	ULib.tsayError( v, "This player is dead or spectating." )
 	
 	end
 		
